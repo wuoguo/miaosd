db06208f346619df8e0921f3167d93daf7ec8d6a
wuoguo <133217640+wuoguo@users.noreply.github.com>
2023-05-11 19:21:39 +0800

    Update mvnw.cmd

--- miaosha-2vsion/mvnw.cmd
+++ miaosha-2vsion/mvnw.cmd
@@ -9,15 +9,7 @@
 @REM
 @REM    http://www.apache.org/licenses/LICENSE-2.0
 @REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
-@REM ----------------------------------------------------------------------------
-
-@REM ----------------------------------------------------------------------------
+@REM Unless---------------------------
 @REM Maven2 Start Up Batch script
 @REM
 @REM Required ENV vars:
