41d99e5aaee3cfc9095dd8563130e2ed6f372322
wuoguo <133217640+wuoguo@users.noreply.github.com>
2023-05-11 20:23:22 +0800

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -8,15 +8,6 @@
 @REM with the License.  You may obtain a copy of the License at
 @REM
 @REM    http://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
-@REM ----------------------------------------------------------------------------
-
 @REM ----------------------------------------------------------------------------
 @REM Maven2 Start Up Batch script
 @REM
